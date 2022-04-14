5203cb977227e77278fab648a223a9bcb4529401
Farshid Sohrabiani <fsohrabi047@gmail.com>
2022-04-15 02:34:24 +0430

    improved task table

--- database/migrations/2022_04_14_180317_create_tasks_table.php
+++ database/migrations/2022_04_14_180317_create_tasks_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('tasks', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id');
+            // $table->foreignId('user_id');
             $table->string('title');
             $table->mediumText('description');
             $table->date('due_date');
