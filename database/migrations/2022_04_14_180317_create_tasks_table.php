67f7a4c930af5e966d3197211eba5864e62704be
Farshid Sohrabiani <fsohrabi047@gmail.com>
2022-04-15 02:08:29 +0430

    create migrations

---
+++ database/migrations/2022_04_14_180317_create_tasks_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id');
+            $table->string('title');
+            $table->mediumText('description');
+            $table->date('due_date');
+            $table->enum('status', ['done', 'in_progress', 'todo'])->default('todo');
+            $table->timestamps();
+
+            $table->foreignId('user_id')
+                ->constrained()
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tasks');
+    }
+};
