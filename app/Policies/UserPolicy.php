e831631487da3fb6b5f5ab0a9011b0f6153c1cc5
Farshid Sohrabiani <fsohrabi047@gmail.com>
2022-04-15 22:41:02 +0430

    implement user policy

--- app/Policies/UserPolicy.php
+++ app/Policies/UserPolicy.php
@@ -13,6 +13,7 @@ class UserPolicy
      * Perform pre-authorization checks.
      *
      * @param \App\Models\User $user 
+     * 
      * @param string $ability 
      * 
      * @return void|bool
@@ -27,7 +28,8 @@ class UserPolicy
     /**
      * Determine whether the user can view any models.
      *
-     * @param  \App\Models\User  $user
+     * @param \App\Models\User $user 
+     * 
      * @return \Illuminate\Auth\Access\Response|bool
      */
     public function viewAny(User $user)
@@ -38,71 +40,69 @@ class UserPolicy
     /**
      * Determine whether the user can view the model.
      *
-     * @param  \App\Models\User  $user
-     * @param  \App\Models\User  $model
+     * @param \App\Models\User $user 
+     * @param \App\Models\User $model 
+     * 
      * @return \Illuminate\Auth\Access\Response|bool
      */
     public function view(User $user, User $model)
     {
-        //
-    }
-
-    /**
-     * Determine whether the user can create models.
-     *
-     * @param  \App\Models\User  $user
-     * @return \Illuminate\Auth\Access\Response|bool
-     */
-    public function create(User $user)
-    {
-        //
+        if ($user->role == 'admin' || $user->id == $model->id) {
+            return true;
+        }
     }
 
     /**
      * Determine whether the user can update the model.
      *
-     * @param  \App\Models\User  $user
-     * @param  \App\Models\User  $model
+     * @param \App\Models\User $user 
+     * @param \App\Models\User $model 
+     * 
      * @return \Illuminate\Auth\Access\Response|bool
      */
     public function update(User $user, User $model)
     {
-        //
+        return ($user->role == 'admin' || $user->id == $model->id)
+            ? true
+            : false;
     }
 
     /**
      * Determine whether the user can delete the model.
      *
-     * @param  \App\Models\User  $user
-     * @param  \App\Models\User  $model
+     * @param \App\Models\User $user 
+     * @param \App\Models\User $model 
+     * 
      * @return \Illuminate\Auth\Access\Response|bool
      */
     public function delete(User $user, User $model)
     {
-        //
+        return $user->role == 'admin' ? true : false;
     }
 
     /**
      * Determine whether the user can restore the model.
      *
-     * @param  \App\Models\User  $user
-     * @param  \App\Models\User  $model
+     * @param \App\Models\User $user 
+     * @param \App\Models\User $model 
+     * 
      * @return \Illuminate\Auth\Access\Response|bool
      */
     public function restore(User $user, User $model)
     {
-        //
+        return $user->role == 'admin' ? true : false;
     }
 
     /**
      * Determine whether the user can permanently delete the model.
      *
-     * @param  \App\Models\User  $user
-     * @param  \App\Models\User  $model
+     * @param \App\Models\User $user 
+     * @param \App\Models\User $model 
+     * 
      * @return \Illuminate\Auth\Access\Response|bool
      */
     public function forceDelete(User $user, User $model)
     {
-        //
+        return $user->role == 'admin' ? true : false;
     }
 }
