fc11dbfd98d65a6a0d8acec84ac04dae30e91e79
Farshid Sohrabiani <fsohrabi047@gmail.com>
2022-04-15 05:45:27 +0430

    added login api

---
+++ app/Http/Controllers/Auth/AuthController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\LoginRequest;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpFoundation\Response;
+
+class AuthController extends Controller
+{
+    /**
+     * Login a user
+     * 
+     * @param Request $request
+     */
+    public function login(LoginRequest $request)
+    {
+        $token = null;
+
+        if (Auth::attempt(['email' => $request->email, 'password' => $request->password])) {
+            $message = __('messages.login.successfully');
+            $statusCode = Response::HTTP_OK;
+
+            $token = $request->user()->createToken($request->token_name);
+        } else {
+            $message = __('messages.login.error');
+            $token = null;
+            $statusCode = Response::HTTP_NOT_FOUND;
+        }
+
+        return response()->json(
+            [
+                'bearer_token' => $token,
+                'message' => $message,
+            ],
+            $statusCode
+        );
+    }
+}
