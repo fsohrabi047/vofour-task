1680b88945c3aff5cd8afe934e1c88e4e3950dc8
Farshid Sohrabiani <fsohrabi047@gmail.com>
2022-04-15 21:27:54 +0430

    create user resource and collection

---
+++ app/Http/Resources/UserResource.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class UserResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param \Illuminate\Http\Request $request 
+     * 
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'email' => $this->email,
+            'role' => $this->role
+        ];
+    }
+
+    /**
+     * Undocumented function
+     *
+     * @param [type] $request 
+     * @param [type] $response 
+     * 
+     * @return void
+     */
+    // public function withResponse($request, $response)
+    // {
+    //     if (is_null($this->resource)) {
+    //         $request->status(404);
+    //     }
+    // }
+}
